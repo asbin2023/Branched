2d0fb9d91fc8e79b4f4538a7046967144f412f71
asbin2023 <asbinasbinasbin2020@gmail.com>
2023-07-31 14:46:22 -0600

    news

--- Mod2/src/components/News.jsx
+++ Mod2/src/components/News.jsx
@@ -3,36 +3,35 @@ import '../styles/News.css'
 
 
 const News = () => {
-    const [news, setNews] = useState([])
-    useEffect(()=>{
-  //  getNews()
-    }, [])
-    async function getNews(){
-        const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_269947ac696999a0b17c8127a9edb2a2980b4&language=en&q=revenu&country=us`)
-        const data = await response.json()
-        let arr = []
-        for (let item of data.results){
-          console.log(item.title.length)
-          if (item.title.length < 70){
-            arr.push(item)
-          } 
-        }
-        console.log(arr)
-        setNews(arr)
-    }
-    console.log(news)
-  return (
-    <div>
-      <h1>LinkedIn News</h1>
-      <ul>
-       <li>News</li>
-       <li>News</li>
-       <li>News</li>
-       <li>News</li>
-       <li>News</li>
-       <li>News</li>
-      </ul>
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    // getNews()
+  }, [])
+  async function getNews() {
+    const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_269947ac696999a0b17c8127a9edb2a2980b4&language=en&q=revenu&country=us`)
+    const data = await response.json()
+    let arr = []
+    for (let item of data.results) {
+      console.log(item.title.length)
 
+      arr.push(item)
+
+    }
+    console.log(arr)
+    arr.splice(6)
+    setNews(arr)
+  }
+  console.log(news)
+  return (
+    <div className="news-container">
+      <h1>Branched News</h1>
+      <ul>
+        {news.length > 0 && news.map(item => {
+          return <li key={crypto.randomUUID()}>
+            <a href={item.link} rel="noreferrer" target="_blank">&bull; {item.title}</a><br /> <br />
+          </li>
+        })}
+      </ul>
     </div>
   )
 }
@@ -41,7 +40,3 @@ export default News
 
 
 
-//   return <li key={crypto.randomUUID()}>
-//   <a href={item.link} rel="noreferrer" target="_blank">{item.title}</a>
-// </li>
-// })}
