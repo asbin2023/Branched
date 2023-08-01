875fbc1cc7ee6dd14e850da9afd3b6539c11f72c
asbin2023 <asbinasbinasbin2020@gmail.com>
2023-08-01 13:31:24 -0600

    finished sign up page

--- Mod2/src/components/Form.jsx
+++ Mod2/src/components/Form.jsx
@@ -65,77 +65,80 @@ const File = () => {
   }
 
   return (
-    <div className="form-display">
-      <div>
-        <h1>Welcome to Branched, Let's get Branching!</h1>
+    <div className="form-main">
+      <div className="form-display">
+        <div>
+          <h1>Welcome to Branched, Let's get Branching!</h1>
+        </div>
+        <form onSubmit={handleUserFormSubmit} action="">
+          <label htmlFor="name">Full Name:</label>
+          <input
+            onChange={handleNameChange}
+            value={nameInput}
+            required
+            type="text"
+            id="name"
+          />
+          <br />
+          <label htmlFor="selectPronouns">Pronouns:</label>
+          <select
+            value={selectedInput}
+            onChange={handleSelectedInputChange}
+            required
+            name="pronouns"
+            id="selectPronouns"
+          >
+            <option value="" hidden>
+              Please select
+            </option>
+            <option value="She/Her">She/Her</option>
+            <option value="He/Him">He/Him</option>
+            <option value="They/Them">They/Them</option>
+            <option value="Ze/Zir">Ze/Zir</option>
+            <option value="Xe/Xem">Xe/Xem</option>
+            <option value="Ve/Ver">Ve/Ver</option>
+            <option value="Ne/Nem">Ne/Nem</option>
+            <option value="Ey/Em">Ey/Em</option>
+            <option value="Other">Other</option>
+          </select>
+          <br />
+          <label htmlFor="headline">Headline:</label>
+          <input
+            onChange={handleHeadlineChange}
+            value={headlineInput}
+            required
+            type="text"
+            id="headline"
+          />
+
+          <br />
+
+          <label htmlFor="location">Location:</label>
+          <input
+            onChange={handleLocationChange}
+            value={locationInput}
+            required
+            type="text"
+            id="location"
+          />
+
+          <br />
+          <label htmlFor="imageFile">Choose your image</label>
+          <input
+            type="file"
+            id="imageFile"
+            required
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          <br />
+          <button type="submit">Submit</button>
+          <button onClick={handleFormReset} type="reset">
+            Clear
+          </button>
+        </form>
       </div>
-      <form onSubmit={handleUserFormSubmit} action="">
-        <label htmlFor="name">Full Name:</label>
-        <input
-          onChange={handleNameChange}
-          value={nameInput}
-          required
-          type="text"
-          id="name"
-        />
-        <br />
-        <label htmlFor="selectPronouns">Pronouns:</label>
-        <select
-          value={selectedInput}
-          onChange={handleSelectedInputChange}
-          required
-          name="pronouns"
-          id="selectPronouns"
-        >
-          <option value="" hidden>
-            Please select
-          </option>
-          <option value="She/Her">She/Her</option>
-          <option value="He/Him">He/Him</option>
-          <option value="They/Them">They/Them</option>
-          <option value="Ze/Zir">Ze/Zir</option>
-          <option value="Xe/Xem">Xe/Xem</option>
-          <option value="Ve/Ver">Ve/Ver</option>
-          <option value="Ne/Nem">Ne/Nem</option>
-          <option value="Ey/Em">Ey/Em</option>
-          <option value="Other">Other</option>
-        </select>
-        <br />
-        <label htmlFor="headline">Headline:</label>
-        <input
-          onChange={handleHeadlineChange}
-          value={headlineInput}
-          required
-          type="text"
-          id="headline"
-        />
-
-        <br />
-
-        <label htmlFor="location">Location:</label>
-        <input
-          onChange={handleLocationChange}
-          value={locationInput}
-          required
-          type="text"
-          id="location"
-        />
-
-        <br />
-        <label htmlFor="imageFile">Choose your image</label>
-        <input
-          type="file"
-          id="imageFile"
-          required
-          accept="image/*"
-          onChange={handleImageChange}
-        />
-        <br />
-        <button type="submit">Submit</button>
-        <button onClick={handleFormReset} type="reset">
-          Clear
-        </button>
-      </form>
+      <img className="hero-img" src="/src/images/svgg.svg" alt="" />
     </div>
   );
 };
